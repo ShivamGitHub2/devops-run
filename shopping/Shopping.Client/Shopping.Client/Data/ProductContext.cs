9c76c1b9bed296d46dfa62289b0377e5d194c528
shivam <shivamrawat2626@gmail.com>
2022-12-09 17:25:30 +0530

    single container added

---
+++ shopping/Shopping.Client/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,35 @@
+﻿using Shopping.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Shopping.Client.Data
+{
+    public static class ProductContext
+    {
+
+        public static readonly List<product> product = new List<product>
+        {
+            new product()
+            {
+                Name="Iphone",
+                Description="this is iphone",
+                ImageFile="product-1.png",
+                Price = 30.00M,
+                Category="smart phone"
+
+            },
+            new product()
+            {
+                Name="Samsung",
+                Description="this is Samsung",
+                ImageFile="product-2.png",
+                Price = 40.00M,
+                Category="smart Tab"
+
+            }
+        };
+    }
+}
